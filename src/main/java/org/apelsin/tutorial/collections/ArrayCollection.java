14c91c3777ec8e725a6c028ec50c032a33b800e2
z.maksudkhonova <zulfiyamaksud@gmail.com>
2022-07-20 12:09:45 +0500

    contains() and containsAll() methods added

--- src/main/java/org/apelsin/tutorial/collections/ArrayCollection.java
+++ src/main/java/org/apelsin/tutorial/collections/ArrayCollection.java
@@ -1,5 +1,7 @@
 package org.apelsin.tutorial.collections;
 
+import java.util.Objects;
+
 public class ArrayCollection {
     private Object[] content;
     private int capacity = 10;
@@ -11,6 +13,35 @@ public class ArrayCollection {
     public ArrayCollection(int capacity){
         this.capacity = capacity;
         content=new Object[capacity];
+    }
+
+    public boolean contains(Object value) {
+        for (int i = 0; i < capacity; i++) {
+            if (Objects.equals(content[i], value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsAll(Object[] values) {
+        for (Object value : values) {
+            if (!contains(value)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void insert(Object item) {
+        if (content.length == capacity) {
+            Object[] newContent = new Object[capacity * 2];
+            for (int i = 0; i < capacity; i++) {
+                newContent[i] = content[i];
+            }
+            content = newContent;
+        }
+        content[capacity++] = item;
     }
 
 }
---
+++ src/test/java/org/apelsin/tutorial/Task6Test.java
@@ -0,0 +1,37 @@
+package org.apelsin.tutorial;
+
+import org.apelsin.tutorial.collections.ArrayCollection;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Task6Test {
+
+    private ArrayCollection initialArray;
+    private Integer[] arrayToCheck1;
+    private Integer[] arrayToCheck2;
+
+
+    @Before
+    public void init(){
+       initialArray = new ArrayCollection() {{
+            insert(1);
+            insert(2);
+            insert(3);
+        }};
+       arrayToCheck1 = new Integer[]{2, 3};
+       arrayToCheck2 = new Integer[]{3, 4, 6};
+    }
+
+    @Test
+    public void containsTest() {
+        Assert.assertTrue(initialArray.contains(2));
+        Assert.assertFalse(initialArray.contains(7));
+    }
+
+    @Test
+    public void containsAllTest() {
+        Assert.assertTrue(initialArray.containsAll(arrayToCheck1));
+        Assert.assertFalse(initialArray.containsAll(arrayToCheck2));
+    }
+}
