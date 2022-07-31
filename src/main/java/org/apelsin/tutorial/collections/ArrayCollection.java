2a7e060daacfab26bed63d8feafc5afc7db43152
Doston Bokhodirov <76911351+dostonbokhodirov@users.noreply.github.com>
2022-08-01 00:55:01 +0500

    addAll() method

--- src/main/java/org/apelsin/tutorial/collections/ArrayCollection.java
+++ src/main/java/org/apelsin/tutorial/collections/ArrayCollection.java
@@ -1,5 +1,6 @@
 package org.apelsin.tutorial.collections;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class ArrayCollection {
@@ -7,18 +8,18 @@ public class ArrayCollection {
     private int capacity = 10;
     private int size;
 
-    public ArrayCollection(){
-        content=new Object[capacity];
+    public ArrayCollection() {
+        content = new Object[capacity];
     }
 
-    public ArrayCollection(int capacity){
+    public ArrayCollection(int capacity) {
         this.capacity = capacity;
-        content=new Object[capacity];
+        content = new Object[capacity];
     }
 
     public boolean contains(Object value) {
         for (int i = 0; i < capacity; i++) {
-            if(value.hashCode() == content[i].hashCode() && Objects.equals(content[i], value) ) {
+            if (value.hashCode() == content[i].hashCode() && Objects.equals(content[i], value)) {
                 return true;
             }
         }
@@ -47,22 +48,41 @@ public class ArrayCollection {
         }
     }
 
-    public Object get(int i){
+    public Object get(int i) {
         Objects.checkIndex(i, size);
         return content[i];
     }
 
-    public boolean isEmpty(){
-        if(Objects.isNull(content)){
+    public boolean isEmpty() {
+        if (Objects.isNull(content)) {
             return true;
         }
         return size == 0;
     }
 
-    public void clear(){
+    public void clear() {
         capacity = 10;
         size = 0;
         this.content = new Object[capacity];
+    }
+
+    public boolean addAll(Object[] values) {
+        Object[] array = Arrays.stream(values).toArray();
+        int arrayLength = array.length;
+        if (arrayLength == 0) return false;
+        Object[] data;
+        final int size2;
+        int lengthValue = (data = this.content).length - (size2 = size);
+        if (arrayLength > lengthValue) data = grow(size2 + arrayLength);
+        System.arraycopy(array, 0, data, size2, arrayLength);
+        size = size2 + arrayLength;
+        return false;
+    }
+
+    private Object[] grow(int minCapacity) {
+        return content.length > 0 || !Arrays.equals(content, new Object[]{})
+                ? (content = Arrays.copyOf(content, minCapacity + capacity / 2))
+                : (content = new Object[Math.max(capacity, minCapacity)]);
     }
 
 }
